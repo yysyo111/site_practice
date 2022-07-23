f9e358e32c6312b3fe87d3d50808a6de53e1f672
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-23 13:14:03 +0000

    [add]userとbookの機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @book = @user.books
   end
 
   def index
+    @users = User.all
   end
 
   def edit
+  end
+
+  def update
   end
 end
