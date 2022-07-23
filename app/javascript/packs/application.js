10e41d7a638a6d94897e204aa2f5ea594107f074
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-23 10:38:35 +0000

    [add]準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,10 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
