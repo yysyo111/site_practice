b5732cbb07490541de14bef63ed7a3245798c030
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-24 02:40:30 +0000

    [add]レイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
-    about_path
+    user_path(resource)
   end
-    
+
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
